9990ef7b9dc3b046f7a397500146ed0919b301b0
kookster <andrew@beginsinwonder.com>
2012-08-24 18:29:52 -0400

    fix apache parser to handle encoded request, and not fail when it does not parse

--- lib/etl/parser/apache_combined_log_parser.rb
+++ lib/etl/parser/apache_combined_log_parser.rb
@@ -18,31 +18,32 @@ module ETL #:nodoc:
       end
       
       def parse(line)
-        # example line:  127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
-        line =~ /^(\S+)\s(\S+)\s(\S+)\s\[([^\]]*)\]\s"([^"]*)"\s(\d*)\s(\S*)\s"([^"]*)"\s"([^"]*)"$/
-        fields = {
-          :ip_address => $1,
-          :identd => $2,
-          :user => $3,
-          :timestamp => $4,
-          :request => $5,
-          :response_code => $6,
-          :bytes => $7.to_i,
-          :referrer => $8,
-          :user_agent => $9,
-        }
-        #fields[:timestamp] =~ r%{(\d\d)/(\w\w\w)/(\d\d\d\d):(\d\d):(\d\d):(\d\d) -(\d\d\d\d)}
-        d = Date._strptime(fields[:timestamp], '%d/%b/%Y:%H:%M:%S') unless fields[:timestamp].nil?
-        fields[:timestamp] = Time.mktime(d[:year], d[:mon], d[:mday], d[:hour], d[:min], d[:sec], d[:sec_fraction]) unless d.nil?
-        
-        fields[:method], fields[:path] = fields[:request].split(/\s/)
 
-        # fields.merge!(parse_user_agent(fields[:user_agent])) unless fields[:user_agent].nil?
-        # fields.merge!(parse_uri(fields[:referrer], :prefix => 'referrer_'))
-        
-        fields.each do |key, value|
-          fields[key] = nil if value == '-'
+        field_names = [:ip_address, :identd, :user, :timestamp, :request, :response_code, :bytes, :referrer, :user_agent]
+
+        fields = field_names.inject({}){|h, n| h[n] = nil; h}
+
+        line = line.gsub('\"', '%22')
+
+        # example line:  127.0.0.1 - frank [10/Oct/2000:13:55:36 -0700] "GET /apache_pb.gif HTTP/1.0" 200 2326 "http://www.example.com/start.html" "Mozilla/4.08 [en] (Win98; I ;Nav)"
+        if match_data = line.match(/^(\S+)\s(\S+)\s(\S+)\s\[([^\]]*)\]\s"([^"]*)"\s(\d*)\s(\S*)\s"([^"]*)"\s"([^"]*)"$/)
+
+          field_names.each_with_index{|n, i| fields[n] = match_data[i+1]}
+
+          fields[:bytes] = fields[:bytes].to_i
+
+          #fields[:timestamp] =~ r%{(\d\d)/(\w\w\w)/(\d\d\d\d):(\d\d):(\d\d):(\d\d) -(\d\d\d\d)}
+          d = Date._strptime(fields[:timestamp], '%d/%b/%Y:%H:%M:%S') unless fields[:timestamp].nil?
+          fields[:timestamp] = Time.mktime(d[:year], d[:mon], d[:mday], d[:hour], d[:min], d[:sec], d[:sec_fraction]) unless d.nil?          
+
+          fields[:method], fields[:path] = fields[:request].split(/\s/) unless fields[:request].nil?
+
+          # fields.merge!(parse_user_agent(fields[:user_agent])) unless fields[:user_agent].nil?
+          # fields.merge!(parse_uri(fields[:referrer], :prefix => 'referrer_')) unless fields[:referrer].nil?
+          
+          fields.each{|k, v| v == '-' ? fields[k] = nil}
         end
+        fields
       end
       
     end
